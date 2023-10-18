76be41dca670e638e4561672384a3f52b5d8b78a
Ken Truong <kjjtruong@gmail.com>
2023-10-18 00:12:10 -0600

    add tailwind and remove homepage content

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `TRECFLOW`,
+    description: `This is TRECFLOW`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
@@ -39,5 +39,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-postcss',
   ],
 }
