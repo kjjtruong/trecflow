d569ddf845dacba9fb37515a2161a62c34f283c4
Ken Truong <kjjtruong@gmail.com>
2023-10-18 23:14:24 -0600

    update header color to black

--- src/components/header.js
+++ src/components/header.js
@@ -17,17 +17,17 @@ function Header() {
 
   return (
     <header className="sticky top-0 z-40">      
-      <div className="flex bg-white flex-wrap items-center justify-between w-screen mx-auto px-4 sm:px-6 lg:px-8 py-4">
+      <div className="flex bg-black flex-wrap items-center justify-between w-screen mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <Link className="no-underline" to="/">        
-          <span className="text-4xl text-black font-bold tracking-wide">{data.site.siteMetadata.title}</span>
+          <span className="text-2xl text-white font-bold tracking-wide">{data.site.siteMetadata.title}</span>
         </Link>
 
         <button
-          className="flex items-center block px-3 py-2 text-black border border-black rounded md:hidden"
+          className="flex items-center block px-3 py-2 text-black border border-white rounded md:hidden"
           onClick={() => toggleExpansion(!isExpanded)}
         >
           <svg
-            className="w-3 h-3 fill-current"
+            className="w-3 h-3 fill-white"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -56,7 +56,7 @@ function Header() {
             },             
           ].map((link) => (
             <Link
-              className="uppercase block mt-4 no-underline md:inline-block md:mt-0 md:ml-6 rounded-full px-3 py-2 text-gray-700 transition-colors delay-150 hover:text-gray-900 hover:bg-gray-100 hover:delay-[0ms]"
+              className="uppercase block mt-4 no-underline md:inline-block md:mt-0 md:ml-6 rounded-full px-3 py-1 text-gray-100 transition-colors delay-150 hover:text-gray-900 hover:bg-gray-100 hover:delay-[0ms]"
               key={link.title}
               to={link.route}
             >
@@ -64,7 +64,7 @@ function Header() {
             </Link>
           ))}
           <a
-            className="uppercase block md:inline-block mt-4 md:mt-0 md:ml-6 no-underline py-3 px-5 rounded-full font-semibold outline-2 outline-offset-2 transition-colors text-zinc-100 bg-black hover:bg-zinc-600 active:bg-zinc-800 active:text-zinc-100"
+            className="uppercase block md:inline-block mt-4 md:mt-0 md:ml-6 no-underline py-1 px-3 rounded-full font-semibold outline-2 outline-offset-2 transition-colors text-zinc-900 bg-white hover:bg-zinc-600 active:bg-white-800 active:text-zinc-100"
             href="mailto:drjan@trecflow.com"    
           >
             Contact Me
--- src/pages/index.js
+++ src/pages/index.js
@@ -70,8 +70,7 @@ const moreLinks = [
 const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
 
 const IndexPage = () => (
-  <Layout>
-    <div className="flex flex-col flex-1 mx-auto w-full"></div>
+  <Layout>    
     {/* Hero */}
     <section className="relatve">
       {/**  Container **/}
